fdf8b392ccd37e22a29b8900229b865ca8bf4e33
Robert Sehr <robert.sehr@intranda.com>
2022-02-17 13:46:01 +0100

    renamed plugin

--- plugin/src/main/java/de/intranda/goobi/plugins/EntityDashboardPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/EntityDashboardPlugin.java
@@ -10,16 +10,16 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 @PluginImplementation
 @Log4j2
-public class SampleDashboardPlugin implements IDashboardPlugin {
+public class EntityDashboardPlugin implements IDashboardPlugin {
 
     @Getter
-    private String title = "intranda_dashboard_sample";
+    private String title = "intranda_dashboard_entity";
 
     @Getter
     private PluginType type = PluginType.Dashboard;
     
     @Getter
-    private String guiPath = "/uii/plugin_dashboard_sample.xhtml";
+    private String guiPath = "/uii/plugin_dashboard_entity.xhtml";
     
     @Getter
     private String value;
@@ -27,8 +27,8 @@ public class SampleDashboardPlugin implements IDashboardPlugin {
     /**
      * Constructor
      */
-    public SampleDashboardPlugin() {
-        log.info("Sample dashboard plugin started");
+    public EntityDashboardPlugin() {
+        log.info("Entity dashboard plugin started");
         value = ConfigPlugins.getPluginConfig(title).getString("value", "default value");
     }   
 
